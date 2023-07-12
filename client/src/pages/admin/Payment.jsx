8d354fc7143a8c059a0754d18ce3345a25ae5e14
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-07-12 18:50:38 +0530

    added payment and shipment page with api integration

--- client/src/pages/admin/Payment.jsx
+++ client/src/pages/admin/Payment.jsx
@@ -1,5 +1,64 @@
+import { Header, ReusableTable } from "../../components";
+import PaymentBanner from "../../assets/images/collectionBannerImg.png";
+import { useEffect, useMemo, useState } from "react";
+import API_WRAPPER from "../../api";
 const Payment = () => {
-  return <div>Payment</div>;
+  const [ordersList, setOrdersList] = useState([]);
+
+  const getOrders = async () => {
+    try {
+      const response = await API_WRAPPER.get("/order/get-orders");
+      setOrdersList(response?.data);
+    } catch (error) {
+      console.error("Error occured while fetching orders");
+    }
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Order ID",
+        accessor: "_id",
+      },
+      {
+        Header: "Total Price",
+        accessor: "total_price",
+      },
+      {
+        Header: "Total Paid",
+        accessor: "total_paid",
+      },
+      {
+        Header: "Payment Method",
+        accessor: "payment_method",
+      },
+      {
+        Header: "Payment Status",
+        accessor: "payment_status",
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(() => ordersList, [ordersList]);
+
+  return (
+    <div>
+      <Header
+        heading="Payments"
+        subheading="This is a Payment page"
+        image={PaymentBanner}
+      />
+
+      <div className="mt-4">
+        <ReusableTable columns={columns} data={data} showButtons={false} />
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
--- client/src/pages/admin/Shipments.jsx
+++ client/src/pages/admin/Shipments.jsx
@@ -1,5 +1,58 @@
-const Shipments = () => {
-  return <div>Shipments</div>;
+import { Header, ReusableTable } from "../../components";
+import PaymentBanner from "../../assets/images/collectionBannerImg.png";
+import { useEffect, useMemo, useState } from "react";
+import API_WRAPPER from "../../api";
+const Payment = () => {
+  const [ordersList, setOrdersList] = useState([]);
+
+  const getOrders = async () => {
+    try {
+      const response = await API_WRAPPER.get("/order/get-orders");
+      setOrdersList(response?.data);
+    } catch (error) {
+      console.error("Error occured while fetching orders");
+    }
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Order ID",
+        accessor: "_id",
+      },
+      {
+        Header: "Shipped Status",
+        accessor: "shipped",
+        // temperorly true is static
+        Cell: (row) => <p>true</p>,
+      },
+      {
+        Header: "Order Status",
+        accessor: "order_status",
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(() => ordersList, [ordersList]);
+
+  return (
+    <div>
+      <Header
+        heading="Shipments"
+        subheading="This is a Payment page"
+        image={PaymentBanner}
+      />
+
+      <div className="mt-4">
+        <ReusableTable columns={columns} data={data} showButtons={false} />
+      </div>
+    </div>
+  );
 };
 
-export default Shipments;
+export default Payment;
