567f910424372729f1a38ef0335261accd2cbad8
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-07-24 18:51:14 +0530

    added autherization middleware to backend

--- client/src/pages/vendor/VendorDashboard.jsx
+++ client/src/pages/vendor/VendorDashboard.jsx
@@ -1,5 +1,13 @@
+import { Header } from "../../components";
 const VendorDashboard = () => {
-  return <div>VendorDashboard</div>;
+  return (
+    <div>
+      <Header
+        heading="Vendor Dashboard"
+        subheading="This subheading exists because its required to provide information about vendor dashboard in banner"
+      />
+    </div>
+  );
 };
 
 export default VendorDashboard;
--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,20 +1,32 @@
-// Verify middleware to protect the API routes
-const verifyToken = (req, res, next) => {
+// middleware/authMiddleware.js
+
+const jwt = require("jsonwebtoken");
+const secretKey = "your_secret_key"; // Replace this with your actual secret key
+
+const authMiddleware = (req, res, next) => {
+  // Get the token from the request headers
   const token = req.header("Authorization");
 
   if (!token) {
-    return res.status(401).json({ error: "Access denied. No token provided." });
+    return res
+      .status(401)
+      .json({ message: "No token found, authorization denied" });
   }
 
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: "Invalid token" });
-    }
+  try {
+    // Verify the token and extract the payload data
+    const decoded = jwt.verify(token, secretKey);
 
-    // Add the decoded user information to the request object for future use
-    req.user = decoded;
+    // You can perform additional checks or attach the user data to the request for future use
+    req.user = decoded.user; // Assuming the token payload contains a 'user' field
+
+    // Call the next middleware or route handler
     next();
-  });
+  } catch (err) {
+    return res
+      .status(401)
+      .json({ message: "Invalid token, authorization denied" });
+  }
 };
 
-module.exports = { verifyToken };
+module.exports = authMiddleware;
