dea9c06e53612dadcb93c2856c273e4a7e86216b
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-07-25 12:55:49 +0530

    added customer crud

--- client/src/pages/admin/Customer.jsx
+++ client/src/pages/admin/Customer.jsx
@@ -1,19 +1,66 @@
-import { useState } from "react";
-import { Header } from "../../components";
-import { debouncedShowToast } from "../../utils";
+import { useEffect, useMemo, useState } from "react";
+import { Header, ReusableTable, Modal } from "../../components";
+import { debouncedShowToast, getStatusStyles } from "../../utils";
 import API_WRAPPER from "../../api";
 
 const Customer = () => {
   const [customerList, setCustomerList] = useState([]);
   const [apiTrigger, setApiTrigger] = useState(false);
-  const [editedCustomer, setEditedCustomer] = useState({});
+  const [selectedRow, setSelectedRow] = useState({});
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "First Name",
+        accessor: "firstName",
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Mobile",
+        accessor: "mobile",
+      },
+      {
+        Header: "Organisation Name",
+        accessor: "organizationName",
+      },
+      {
+        Header: "Country",
+        accessor: "country",
+      },
+      {
+        Header: "City",
+        accessor: "city",
+      },
+      {
+        Header: "Pin Code",
+        accessor: "pincode",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+        Cell: ({ row }) => {
+          return getStatusStyles(row?.original?.status);
+        },
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(() => customerList, [customerList]);
 
   const getAllCustomers = async () => {
     try {
       const response = await API_WRAPPER.get("customers/get-customers");
       if (response.status === 200) {
-
-        
+        setCustomerList(response?.data?.customers);
+        console.log("CUSTOMER DATA: ", response?.data);
         debouncedShowToast("Successfully loaded customer list", "success");
       }
     } catch (error) {
@@ -21,11 +68,127 @@ const Customer = () => {
     }
   };
 
+  const deleteCustomer = async () => {
+    try {
+      const response = await API_WRAPPER.delete(
+        `/customers/delete-customer/:${selectedRow?._id}`
+      );
+    } catch (error) {
+      debouncedShowToast(error.message, "error");
+    }
+  };
+
+  const updateCustomer = async (updatedObj) => {
+    try {
+      const response = await API_WRAPPER.put(
+        `customers/update-customer/:${selectedRow?._id}`,
+        updatedObj
+      );
+      if (response.status === 200) {
+        setApiTrigger((prevState) => !prevState);
+      }
+    } catch (error) {
+      debouncedShowToast(error.message, "error");
+    }
+  };
+
+  const handleDelete = (row) => {
+    console.log("ROW TO BE DELETED: ", row);
+    setSelectedRow(row);
+  };
+
+  const handleEdit = (row) => {
+    window.edit_customer_modal.showModal();
+    console.log("ROW TO BE EDITED: ", row);
+    setSelectedRow(row);
+  };
+  const handleDeleteModalClose = () => {
+    return window.edit_customer_modal.close();
+  };
+
+  const handleSave = (inputValues) => {
+    console.log("SAVING THE INPUT VALUES: ", inputValues);
+    updateCustomer(inputValues);
+  };
+
+  useEffect(() => {
+    getAllCustomers();
+  }, [apiTrigger]);
+
   return (
     <div>
       <Header
         heading="Customer"
         subheading="This is a customers page which provides information regarding all the customers that are present in the application"
+      />
+      <div className="mt-4">
+        <ReusableTable
+          columns={columns}
+          data={data}
+          showButtons
+          enableDelete
+          enableEdit
+          onDelete={handleDelete}
+          onEdit={handleEdit}
+        />
+      </div>
+      <Modal
+        id="edit_customer_modal"
+        title="Are you sure you want to delete the selected value?"
+        onClose={handleDeleteModalClose}
+        onSave={handleSave}
+        defaultValues={{
+          firstName: selectedRow?.firstName,
+          lastName: selectedRow?.lastName,
+          email: selectedRow?.email,
+          mobile: selectedRow?.mobile,
+          organizationName: selectedRow?.organizationName,
+          country: selectedRow?.country,
+          city: selectedRow?.city,
+          pincode: selectedRow?.pincode,
+        }}
+        inputs={[
+          {
+            label: "First Name",
+            type: "text",
+            name: "firstName",
+          },
+          {
+            label: "Last Name",
+            type: "text",
+            name: "lastName",
+          },
+          {
+            label: "Email",
+            type: "text",
+            name: "email",
+          },
+          {
+            label: "Mobile",
+            type: "tel",
+            name: "mobile",
+          },
+          {
+            label: "Organisation Name",
+            type: "text",
+            name: "organizationName",
+          },
+          {
+            label: "Country",
+            type: "text",
+            name: "country",
+          },
+          {
+            label: "City",
+            type: "text",
+            name: "city",
+          },
+          {
+            label: "Pin Code",
+            type: "text",
+            name: "pincode",
+          },
+        ]}
       />
     </div>
   );
