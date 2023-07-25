fab1f46d755761d5d0fec42bb41f8af914c0c2b3
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-07-25 12:02:58 +0530

    added default configuration to vendor product inventory

--- client/src/pages/vendor/VendorProductInventory.jsx
+++ client/src/pages/vendor/VendorProductInventory.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useMemo, useState } from "react";
 import { Header } from "../../components";
 
 const VendorProductInventory = () => {
+  const [productsList, setProductsList] = useState([]);
+  const [apiTrigger, setApiTrigger] = useState(false);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Product Name",
+        accessor: "name",
+      },
+      {
+        Header: "Product Price",
+        accessor: "price",
+      },
+      {
+        Header: "Stock Quantity",
+        accessor: "stockQuantity",
+      },
+      {
+        Header: "Stock Status",
+        accessor: "stockStatus",
+      },
+    ],
+    []
+  );
+  const data = useMemo(() => productsList, [productsList]);
+
+  const getAllProducts = () => {};
+
+  useEffect(() => {
+    getAllProducts();
+  });
+
   return (
     <div>
       <Header
