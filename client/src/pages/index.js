bccb31b7728e582530eb55cb51452bdfff9a5f54
Pratul <pratul.udainiya@rechargestudio.com>
2023-08-01 15:50:20 +0530

    added changes

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -31,5 +31,5 @@ export { default as AddCategory } from "./admin/AddCategory";
 export { default as AddAttributes } from "./admin/AddAttributes";
 export { default as Attribute } from "./admin/Attribute";
 export { default as AddDiscount } from "./admin/AddDiscount";
-export { default as AddCoupon } from "./admin/addCoupon";
+export { default as AddCoupon } from "./admin/AddCoupon";
 export { default as AddCollection } from "./admin/AddCollection";
