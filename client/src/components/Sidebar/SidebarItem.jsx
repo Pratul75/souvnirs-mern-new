8490f567088c4666f02d97ae8d99fba7a1ecec44
utkarsh pawar <utkarshpawar910910@gmail.com>
2023-08-03 19:00:29 +0530

    changes in sales tabs

--- client/src/components/Sidebar/SidebarItem.jsx
+++ client/src/components/Sidebar/SidebarItem.jsx
@@ -2,12 +2,14 @@ import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 import { sidebarVariant } from "../../animation";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setActiveLink } from "../../features/appConfig/appSlice";
 
 const SidebarItem = ({ title, navLink, Icon, sidebarState }) => {
   const dispatch = useDispatch();
+  const darkMode = useSelector(state => state.appConfig.darkMode)
 
+  const col = darkMode ? "bg-gray-800" : "bg-[#4680ff36]"
   return (
     <li className="mx-2 cursor-pointer">
       <NavLink
@@ -15,21 +17,18 @@ const SidebarItem = ({ title, navLink, Icon, sidebarState }) => {
         to={navLink}
         className={({ isActive }) =>
           isActive
-            ? `flex text-themeColor  bg-gray-800 rounded-xl  w-full p-4 ${
-                sidebarState ? "justify-start" : "justify-center"
-              } `
-            : `flex hover:bg-base-300 rounded-xl w-full p-4 transition-all ease-in-out duration-300 ${
-                sidebarState ? "justify-start" : "justify-center"
-              } `
+            ? `flex text-themeColor  ${col} rounded-xl  w-full p-4 ${sidebarState ? "justify-start" : "justify-center"
+            } `
+            : `flex hover:bg-base-300 rounded-xl w-full p-4 transition-all ease-in-out duration-300 ${sidebarState ? "justify-start" : "justify-center"
+            } `
         }
       >
         <div className="flex flex-row gap-2 items-center">
           {Icon && <Icon />} {/* Only render Icon if it exists */}
           {sidebarState && title && (
             <motion.span
-              className={`ml-2 text-xs font-semibold ${
-                sidebarState ? "block" : "hidden"
-              }`}
+              className={`ml-2 text-xs font-semibold ${sidebarState ? "block" : "hidden"
+                }`}
               variants={sidebarVariant}
               expanded="expanded"
               collapsed="collapsed"
