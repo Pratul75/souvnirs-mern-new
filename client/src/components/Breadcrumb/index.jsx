4257a04755bb3400b3662a04e2a59b54caa0878d
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-08-05 12:52:13 +0530

    added animation to numbers

--- client/src/components/Breadcrumb/index.jsx
+++ client/src/components/Breadcrumb/index.jsx
@@ -1,4 +1,3 @@
-import { nanoid } from "nanoid";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
--- client/src/components/DashboardCard/index.jsx
+++ client/src/components/DashboardCard/index.jsx
@@ -1,6 +1,8 @@
-import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 import Card from "../Card";
+
 const DashboardCard = ({
   number,
   subheading,
@@ -8,18 +10,34 @@ const DashboardCard = ({
   iconColor,
   textColor,
 }) => {
+  const [animatedNumber, setAnimatedNumber] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (animatedNumber < number) {
+        setAnimatedNumber(animatedNumber + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 20);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [number, animatedNumber]);
+
   return (
     <Card>
       <motion.div className="w-full flex justify-between items-center py-4 px-6">
         <div className="flex flex-col">
           <h2 className="text-xl font-bold md:text-2xl lg:text-3xl">
-            {number}+
+            {animatedNumber}+
           </h2>
           <p className={`text-xs md:text-sm ${textColor}`}>{subheading}</p>
         </div>
 
         <div
-          className={` p-2 flex justify-center items-center rounded-xl ${iconColor}`}
+          className={`p-2 flex justify-center items-center rounded-xl ${iconColor}`}
         >
           {iconSvg}
         </div>
