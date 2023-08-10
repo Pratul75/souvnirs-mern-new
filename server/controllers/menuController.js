320a01a7a0e60810bca5b4b7c575edd3435e4425
utkarsh pawar <utkarshpawar910910@gmail.com>
2023-08-10 13:25:47 +0530

    added safety check for duplicate title

--- server/controllers/menuController.js
+++ server/controllers/menuController.js
@@ -1,7 +1,12 @@
+const { response } = require("express");
 const Menu = require("../schema/menuItem");
 
 const createMenu = async () => {
   const { title } = req.body;
+  const titleExists = await Menu.findOne({ title });
+  if (titleExists) {
+    response.status(400).json("title Already exist");
+  }
   const createdMenu = await Menu.create({ title });
   res.status(200).json(createdMenu);
 };
