70e35d643e816fd124679c68ec4deab578dc7c8e
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-08-12 11:16:31 +0530

    added manu and its nesting in navbar for landing page

--- client/src/components/shop/headers/ShopNavbar.jsx
+++ client/src/components/shop/headers/ShopNavbar.jsx
@@ -1,86 +1,56 @@
-import { AiOutlineUnorderedList } from "react-icons/ai";
 import { BsChevronDown } from "react-icons/bs";
 import { FaHeadphones } from "react-icons/fa";
 import API_WRAPPER from "../../../api";
 import { useEffect, useState } from "react";
-
 const ShopNavbar = () => {
   const [navbarData, setNavbarData] = useState([]);
 
   const getNavbarData = async () => {
     const response = await API_WRAPPER.get("/getNavbarMenu");
     if (response.status === 200) {
-      setNavbarData(response.data[0].mainMenu);
-      console.log("RESPONSE DATA", response.data[0].mainMenu);
+      setNavbarData(response.data);
     }
   };
-  console.log("ShopNavbar.jsx", navbarData);
+
   useEffect(() => {
     getNavbarData();
   }, []);
+
+  const renderSubMenu = (subMenu) => {
+    return (
+      <ul className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+        {subMenu.map((submenu) => (
+          <li key={submenu._id}>
+            <a className="font-bold">{submenu.title}</a>
+            {submenu.child && submenu.child.length > 0 && (
+              <ul className="sub-submenu">
+                {submenu.child.map((child) => (
+                  <li key={child._id}>
+                    <a>{child.title}</a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div className="mx-16 flex justify-between w-full border-[1px] bg-base-200">
-      {/* <div className="join">
-        <button className="bg-primary rounded-none p-4 text-white flex items-center justify-between gap-2 px-8 join-item">
-          <AiOutlineUnorderedList className="text-xl" />
-          All Departments
-          <BsChevronDown className="text-md ml-1" />
-        </button>
-        <button className="rounded-none  p-4 text-black flex justify-center gap-2  join-item">
-          Home
-        </button>
-        <button className="rounded-none  p-4 text-black flex justify-center gap-2  join-item">
-          About
-        </button>
-        <button className="rounded-none  p-4 text-black flex justify-center gap-2  join-item">
-          Shop
-        </button>
-        <button className="rounded-none  p-4 text-black flex justify-center gap-2  join-item">
-          Pages
-        </button>
-        <button className="rounded-none  p-4 text-black flex justify-center gap-2  join-item">
-          Blog
-        </button>
-        <button className="rounded-none  p-4 text-black flex justify-center gap-2  join-item">
-          Contact
-        </button>
-      </div> */}
       <div className="join">
-        {navbarData?.map((mainmenu) => {
-          console.log("ShopNavbar.jsx", mainmenu);
-          return (
-            <div className="dropdown join-item">
-              <label tabIndex={0} className="btn m-1">
-                {mainmenu.title}
-              </label>
-
-              <ul
-                tabIndex={0}
-                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a>{mainmenu.submenus.title}</a>
-                </li>
-                <li>
-                  <a>Item 2</a>
-                </li>
-              </ul>
-            </div>
-          );
-        })}
-      </div>
-
-      <div className="flex items-center">
-        <div className="px-4 text-sm">Request Quote</div>
-        <div className="border-l-[1px] flex items-center justify-between gap-4 px-4 border-base-300">
-          <FaHeadphones className="text-primary text-4xl" />
-          <div>
-            <h5 className="text-xs">CALL US 24/7</h5>
-            <h5 className="text-primary text-xs cursor-pointer">
-              +00 123 456 789
-            </h5>
+        {navbarData?.map((mainmenu) => (
+          <div key={mainmenu._id} className="dropdown join-item">
+            <label tabIndex={0} className="btn m-1">
+              {mainmenu.title}
+              <BsChevronDown className="ml-1" />
+            </label>
+            {mainmenu.submenus &&
+              mainmenu.submenus.length > 0 &&
+              renderSubMenu(mainmenu.submenus)}
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
