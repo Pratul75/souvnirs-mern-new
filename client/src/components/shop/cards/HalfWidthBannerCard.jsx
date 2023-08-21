33c9e5ad8ff692791fe10b6b90d000e576c2c775
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-08-21 15:51:35 +0530

    added changes to halfWidthBannerCard

--- client/src/components/shop/cards/HalfWidthBannerCard.jsx
+++ client/src/components/shop/cards/HalfWidthBannerCard.jsx
@@ -15,15 +15,11 @@ const HalfWidthBannerCard = ({
   return (
     <div className="grid grid-cols-2 gap-4 mt-16">
       <div className="col-span-2 md:col-span-1 relative">
-        <img
-          className="object-cover rounded-xl w-full"
-          src={backgroundImageOne}
-          alt=""
-        />
-        <div className="inset-0 absolute flex justify-between items-center px-16">
+        <img className="rounded-xl w-full" src={backgroundImageOne} alt="" />
+        <div className="inset-0 absolute flex justify-between items-center px-16 ">
           <div className="flex flex-col  justify-center">
             <h1 className="text-2xl">{headingOne}</h1>
-            <h2 className="text-7xl  font-bold">
+            <h2 className="text-[60px]  font-bold leading-[60px]">
               {cardTitleOne.toUpperCase()}
             </h2>
             <button
@@ -34,9 +30,10 @@ const HalfWidthBannerCard = ({
             </button>
           </div>
           <img
-            className="mr-20"
+            className=" object-contain"
             style={{
               mixBlendMode: "multiply",
+              width: "70%",
             }}
             src={productImageOne}
             alt=""
@@ -44,15 +41,11 @@ const HalfWidthBannerCard = ({
         </div>
       </div>
       <div className="col-span-2 md:col-span-1 relative">
-        <img
-          className="object-cover rounded-xl w-full"
-          src={backgroundImageOne}
-          alt=""
-        />
+        <img className="rounded-xl w-full" src={backgroundImageOne} alt="" />
         <div className="inset-0 absolute flex justify-between items-center px-16">
           <div className="flex flex-col  justify-center">
             <h1 className="text-2xl">{headingOne}</h1>
-            <h2 className="text-6xl  font-bold">
+            <h2 className="text-[60px]  font-bold  leading-[60px]">
               {cardTitleOne.toUpperCase()}
             </h2>
             <button
@@ -63,9 +56,10 @@ const HalfWidthBannerCard = ({
             </button>
           </div>
           <img
-            className="mr-20"
+            className=" object-contain"
             style={{
               mixBlendMode: "multiply",
+              width: "70%",
             }}
             src={productImageOne}
             alt=""
