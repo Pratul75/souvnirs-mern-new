40041f7031572e57a97305977e12e6740d3af6a1
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-08-23 14:15:45 +0530

    added skeleton for productsListWithFILTERS

--- client/src/components/shop/components/ProductsListWithFilters.jsx
+++ client/src/components/shop/components/ProductsListWithFilters.jsx
@@ -2,15 +2,22 @@ import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { ProductCard, ScrollAnimationWrapper } from "../../../components";
 import API_WRAPPER from "../../../api";
-const ProductsListWithFilters = ({ heading, filters, products }) => {
-  const [activeFilter, setActiveFilter] = useState(filters[0].id); // Set the initial active filter
-  const [productsList, setProductsList] = useState([]);
-  const [apiTrigger, setApiTrigger] = useState(false);
 
-  // Todo: to be used once api call is triggered
-  // const filteredProducts = products.filter(
-  //   (product) => product.filterId === activeFilter
-  // );
+const SkeletonProductCard = () => {
+  return (
+    <div className="animate-pulse p-4 border border-gray-300 rounded-md w-full md:w-64">
+      <div className="bg-gray-300 h-44 w-full mb-2 rounded-md"></div>
+      <div className="h-4 w-1/2 bg-gray-300 mb-2 rounded-md"></div>
+      <div className="h-4 w-1/4 bg-gray-300 mb-2 rounded-md"></div>
+    </div>
+  );
+};
+
+const ProductsListWithFilters = ({ heading, filters, products }) => {
+  const [activeFilter, setActiveFilter] = useState(filters[0].id);
+  const [productsList, setProductsList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   function getRandomValues(array) {
     if (array.length <= 5) {
       return array.slice();
@@ -37,15 +44,17 @@ const ProductsListWithFilters = ({ heading, filters, products }) => {
       if (response.status === 200) {
         console.log("PRODUCT DATA: ", response?.data);
         setProductsList(getRandomValues(response?.data));
+        setLoading(false); // Set loading to false after data is fetched
       }
     } catch (error) {
       console.error(error.message);
+      setLoading(false); // Set loading to false in case of error
     }
   };
 
   useEffect(() => {
     getAllProducts();
-  }, [apiTrigger]);
+  }, []);
 
   return (
     <ScrollAnimationWrapper>
@@ -63,7 +72,8 @@ const ProductsListWithFilters = ({ heading, filters, products }) => {
                 key={filter.id}
                 onClick={() => {
                   setActiveFilter(filter.id);
-                  setApiTrigger((prevState) => !prevState);
+                  setLoading(true); // Reset loading when filter changes
+                  getAllProducts();
                 }}
               >
                 {filter.name}
@@ -73,21 +83,28 @@ const ProductsListWithFilters = ({ heading, filters, products }) => {
         </div>
         <hr />
         <div className="flex justify-center md:justify-between gap-4 py-4 flex-wrap md:flex-nowrap">
-          {productsList.map(({ _id, name, price, rating, coverImage }) => (
-            <ProductCard
-              key={_id}
-              title={name}
-              price={price}
-              rating={4.3}
-              image={coverImage}
-            />
-          ))}
+          {loading
+            ? // Render skeleton cards while loading
+              Array.from({ length: 5 }).map((_, index) => (
+                <SkeletonProductCard key={index} />
+              ))
+            : // Render actual product cards when data is available
+              productsList.map(({ _id, name, price, rating, coverImage }) => (
+                <ProductCard
+                  key={_id}
+                  title={name}
+                  price={price}
+                  rating={4.3}
+                  image={coverImage}
+                />
+              ))}
         </div>
       </div>
     </ScrollAnimationWrapper>
   );
 };
 
+// PropTypes and export remain unchanged
 ProductsListWithFilters.propTypes = {
   heading: PropTypes.string.isRequired,
   filters: PropTypes.arrayOf(
