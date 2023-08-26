d5eaee1ec6f36efb52318f18b522db4b77723660
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-08-26 16:08:03 +0530

    added changes to the following code

--- client/src/components/shop/headers/ShopNavbar.jsx
+++ client/src/components/shop/headers/ShopNavbar.jsx
@@ -1,23 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import API_WRAPPER from "../../../api";
-import { nanoid } from "nanoid";
-import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
 const ShopNavbar = () => {
-  const variants = {
-    hidden: { opacity: 0, y: -20 },
-    animate: {
-      opacity: 1,
-      y: 0,
-    },
-  };
-
   const [menuData, setMenuData] = useState([]);
-  const [dropdownToggle, setDropdownToggle] = useState(false);
-  const [toggledMenuItem, setToggledMenuItem] = useState({});
-
-  const menuRef = useRef(null); // Ref for the toggled menu
-
   const getNavbarData = async () => {
     try {
       const response = await API_WRAPPER.get("/getNavbarMenu");
@@ -29,102 +13,67 @@ const ShopNavbar = () => {
       console.error("Error fetching navbar data:", error);
     }
   };
-
-  const handleDropdownEnter = (menuItem) => {
-    setDropdownToggle(true);
-    setToggledMenuItem(menuItem);
-    console.log("SELECTED MENU ITEM:", menuItem);
-  };
-
-  const handleDropdownLeave = () => {
-    // setDropdownToggle(false);
-  };
-
-  const handleDocumentClick = () => {
-    setDropdownToggle(false);
-  };
-
-  useEffect(() => {
-    getNavbarData();
-    window.addEventListener("click", handleDocumentClick);
-
-    return () => {
-      window.removeEventListener("click", handleDocumentClick);
-    };
-  }, []);
-
   useEffect(() => {
     getNavbarData();
   }, []);
 
-  useEffect(() => {
-    console.log("TOGGLED MENU ITEM :", toggledMenuItem.submenus);
-  }, [dropdownToggle, toggledMenuItem]);
+  const renderChildMenu = (childMenu) => {
+    return (
+      <ul className="pl-4 mt-2 space-y-1">
+        {childMenu.map((childItem) => (
+          <li key={childItem._id} className="hover:text-blue-500">
+            <a href={childItem.link}>{childItem.title}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderSubMenu = (subMenu) => {
+    return (
+      <div className="py-2 px-4 w-96 grid grid-cols-2 bg-gray-100">
+        <details className="">
+          <summary className="font-semibold">{subMenu.title}</summary>
+          {subMenu.child.length > 0 && renderChildMenu(subMenu.child)}
+        </details>
+      </div>
+    );
+  };
+
+  const renderMenu = (menu) => {
+    return (
+      <li key={menu._id} className="relative group">
+        <a
+          href={menu.link}
+          className="block py-2 px-4 hover:bg-blue-100 transition duration-300"
+        >
+          {menu.title}
+        </a>
+        {menu.submenus.length > 0 && (
+          <div className="hidden group-hover:block absolute left-full top-0 mt-1 bg-white border border-gray-300 shadow-lg z-10 p-2 rounded-lg">
+            {menu.submenus.map((subMenu) => (
+              <div key={subMenu._id}>
+                <a
+                  href={subMenu.link}
+                  className="block py-2 px-4 hover:bg-blue-100 transition duration-300"
+                >
+                  {subMenu.child.length > 0 ? null : subMenu.title}
+                </a>
+                {subMenu.child.length > 0 && renderSubMenu(subMenu)}
+              </div>
+            ))}
+          </div>
+        )}
+      </li>
+    );
+  };
 
   return (
-    <div className="relative">
-      <div className="navbar bg-base-200 p-0">
-        <div className="navbar-start">
-          <a className="text-lg px-2 bg-shopPrimaryColor py-[16.5px] h-full text-white">
-            All Categories
-          </a>
-        </div>
-        <div className="navbar-center gap-4">
-          {menuData?.map((menuItem) => (
-            <div
-              onMouseEnter={() => handleDropdownEnter(menuItem)}
-              onMouseLeave={handleDropdownLeave}
-              key={nanoid()}
-            >
-              <h1 className="text-xs">{menuItem.title}</h1>
-              <div></div>
-            </div>
-          ))}
-        </div>
-        <div className="navbar-end"></div>
+    <nav className="bg-gray-200 py-4 flex items-center">
+      <div className="container mx-auto">
+        <ul className="flex ">{menuData.map((menu) => renderMenu(menu))}</ul>
       </div>
-
-      {dropdownToggle && (
-        <div className="w-full z-50 flex justify-center fixed  left-0 right-0 transition">
-          <div
-            id="toggled-menu"
-            className=" p-4 bg-violet-400   mt-4 rounded-xl px-16 w-3/4 "
-          >
-            <div className="grid grid-cols-5">
-              {toggledMenuItem.submenus.map((submenu) => {
-                return (
-                  <div key={nanoid()}>
-                    <Link
-                      to={submenu.link}
-                      className="text-md hover:bg-base-100 text-shopPrimaryColor rounded-full px-2 py-1 cursor-pointer"
-                    >
-                      {submenu.title}
-                    </Link>
-                    <div>
-                      {submenu.child && (
-                        <div className="flex flex-col gap-2">
-                          {submenu.child.map((childItem) => {
-                            return (
-                              <Link
-                                to={childItem.link}
-                                className="text-xs pl-2 px-2 py-1 hover:bg-base-200 hover:rounded-lg cursor-pointer text-white hover:text-shopPrimaryColor"
-                                key={nanoid()}
-                              >
-                                {childItem.title}
-                              </Link>
-                            );
-                          })}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    </nav>
   );
 };
 
