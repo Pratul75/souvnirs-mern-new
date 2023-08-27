313809d863e4a924cb6e03ce62b839d3c40f85b9
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-08-27 13:07:11 +0530

    added changes to shopNav

--- client/src/components/shop/headers/ShopNavbar.jsx
+++ client/src/components/shop/headers/ShopNavbar.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import API_WRAPPER from "../../../api";
 import { motion } from "framer-motion";
+
 const ShopNavbar = () => {
   const [menuData, setMenuData] = useState([]);
   const fadeInVariants = {
@@ -10,22 +11,6 @@ const ShopNavbar = () => {
       duration: 1000,
     },
   };
-
-  const getNavbarData = async () => {
-    try {
-      const response = await API_WRAPPER.get("/getNavbarMenu");
-      if (response.status === 200) {
-        setMenuData(response.data);
-        console.log("NAVBAR DATA:", response.data);
-      }
-    } catch (error) {
-      console.error("Error fetching navbar data:", error);
-    }
-  };
-
-  useEffect(() => {
-    getNavbarData();
-  }, []);
 
   const renderChildMenu = (childMenu) => (
     <ul className="pl-4 mt-2 space-y-1">
@@ -63,7 +48,7 @@ const ShopNavbar = () => {
         {menu.title}
       </a>
       {menu.submenus.length > 0 && (
-        <div className="hidden group-hover:block w-96 absolute left-full top-0 mt-1 z-10 p-2 rounded-lg bg-white shadow-xl">
+        <div className="hidden group-hover:block w-[1200px] absolute top-0 mt-1 z-10 p-2 rounded-lg bg-white shadow-xl">
           <div className="flex items-center justify-between flex-wrap">
             {menu.submenus.map((subMenu) => (
               <div key={subMenu._id} className="w-1/4 p-2 ">
