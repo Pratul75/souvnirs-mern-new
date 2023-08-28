82226b5412b367a108ab3615d63a0728f80e6b2d
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-08-28 11:17:54 +0530

    added changes to mega menu

--- client/src/components/shop/headers/ShopNavbar.jsx
+++ client/src/components/shop/headers/ShopNavbar.jsx
@@ -1,66 +1,53 @@
 import { Menu } from "antd";
-import {
-  UserOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  FullscreenExitOutlined,
-  FullscreenOutlined,
-} from "@ant-design/icons";
-
-const MegaMenu = () => {
-  return <>{/* render menus here */}</>;
-};
+import API_WRAPPER from "../../../api";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 const ShopNavbar = () => {
+  // navbar data stored here in navbarData state
+  const [navbarData, setNavbarData] = useState([]);
+  const getNavbarData = async () => {
+    const response = await API_WRAPPER.get("/getNavbarMenu");
+    if (response.status === 200) {
+      setNavbarData(response.data);
+      console.log("NAVBAR DATA: ", response.data);
+    }
+  };
+
+  useEffect(() => {
+    getNavbarData();
+  }, []);
+
+  // Recursive function to render nested submenus
+  const renderSubmenus = (submenus) => {
+    return submenus.map((submenu) => (
+      <Link to={`${window.location.origin}/${submenu.link}`} key={submenu._id}>
+        <Menu.SubMenu
+          title={submenu.title}
+          // icon={submenu.child && submenu.child.length > 0 ? submenu.icon : null}
+        >
+          {submenu.child && submenu.child.length > 0
+            ? renderSubmenus(submenu.child) // Render nested child menus
+            : null}
+        </Menu.SubMenu>
+      </Link>
+    ));
+  };
+
   return (
     <div>
-      <Menu
-        mode="horizontal"
-        items={[
-          {
-            label: "Fashion",
-            key: "fashion",
-            icon: <ShopOutlined />,
-            children: [
-              {
-                label: "Men",
-                key: "mens",
-                icon: <UserOutlined />,
-              },
-              {
-                label: "Women",
-                key: "women",
-                icon: <UserOutlined />,
-              },
-              {
-                label: "Kids",
-                key: "kids",
-                icon: <TeamOutlined />,
-              },
-            ],
-          },
-          {
-            label: "Home Decor",
-            key: "home_decor",
-            children: [
-              {
-                label: "Interior Decor",
-                key: "interior-decor",
-                icon: <FullscreenExitOutlined />,
-              },
-              {
-                label: "Exterior Decor",
-                key: "exterior-decor",
-                icon: <FullscreenOutlined />,
-              },
-            ],
-          },
-          {
-            label: "Electronics",
-            key: "electronics",
-            children: [{ label: <MegaMenu />, key: "megamenu" }],
-          },
-        ]}
-      ></Menu>
+      <Menu mode="horizontal">
+        {navbarData.map((menu) => (
+          <Menu.SubMenu
+            key={menu._id}
+            title={menu.title}
+            // icon={menu.submenus && menu.submenus.length > 0 ? menu.icon : null}
+          >
+            {menu.submenus && menu.submenus.length > 0
+              ? renderSubmenus(menu.submenus) // Render submenus with nested children
+              : null}
+          </Menu.SubMenu>
+        ))}
+      </Menu>
     </div>
   );
 };
