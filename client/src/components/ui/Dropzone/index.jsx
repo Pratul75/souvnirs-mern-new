152085d895c19c89a3e5d820f38132732b8b9f7d
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-09-07 18:37:01 +0530

    added chjanges to overlay

--- client/src/components/ui/Dropzone/index.jsx
+++ client/src/components/ui/Dropzone/index.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
-import { AiOutlineCloseCircle } from "react-icons/ai";
 import { BsUpload } from "react-icons/bs";
 
 const Dropzone = ({ onFilesChange }) => {
@@ -24,17 +23,9 @@ const Dropzone = ({ onFilesChange }) => {
     [files, onFilesChange]
   );
 
-  // const removeFile = (fileName) => {
-  //   // Use the functional form of setFiles to ensure the most up-to-date value of files
-  //   setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
-  //   onFilesChange((prevFiles) =>
-  //     prevFiles.filter((file) => file.name !== fileName)
-  //   );
-  // };
-
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
-    accept: "image/png", // Accept all image types
+    accept: "image/png",
   });
 
   return (
@@ -54,22 +45,6 @@ const Dropzone = ({ onFilesChange }) => {
           </div>
         </div>
       )}
-      <div className="flex flex-col gap-1">
-        {/* {files?.map((file) => (
-          <div
-            key={file.name}
-            className="flex items-center bg-accent rounded-xl p-4"
-          >
-            <p>{file.name}</p>
-            <button
-              className="ml-2 text-red-600 hover:text-red-800"
-              onClick={() => removeFile(file.name)}
-            >
-              <AiOutlineCloseCircle />
-            </button>
-          </div>
-        ))} */}
-      </div>
       {/* Modal to inform the user */}
       {showModal && (
         <div className="modal">
