1b96cd6e6109fefaef283ee68ba0aedfc8e60df7
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-09-14 14:11:17 +0530

    added more hooks

--- client/src/pages/admin/AddCustomer/AddCustomer.jsx
+++ client/src/pages/admin/AddCustomer/AddCustomer.jsx
@@ -1,53 +1,9 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { FormInput, Header } from "../../components";
-import API_WRAPPER from "../../api";
-import { debouncedShowToast } from "../../utils";
+import { FormInput, Header } from "../../../components";
 import { ToastContainer } from "react-toastify";
-const schema = yup.object().shape({
-  firstName: yup.string().required("First name is required"),
-  lastName: yup.string().required("Last name is required"),
-  email: yup.string().email("Invalid email").required("Email is required"),
-  mobile: yup.string().required("Mobile number is required"),
-  password: yup.string().required("Password is required"),
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref("password"), null], "Passwords must match")
-    .required("Confirm Password is required"),
-  organizationName: yup.string().required("Organization name is required"),
-  organizationType: yup.string().required("Organization type is required"),
-  country: yup.string().required("Country is required"),
-  city: yup.string().required("City is required"),
-  pincode: yup.string().required("Pincode is required"),
-});
+import useAddCustomer from "./useAddCustomer";
 
 const AddCustomer = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit = async (data) => {
-    const { confirmPassword, ...formData } = data;
-    formData.password = confirmPassword;
-    console.log("CUSTOMER FORM DATA: ", formData);
-    try {
-      const response = await API_WRAPPER.post(
-        "/auth/register/customer",
-        formData
-      );
-      if (response.status === 200) {
-        debouncedShowToast("Customer added successfully", "success");
-      }
-    } catch (error) {
-      debouncedShowToast(error.message, "error");
-    }
-  };
-
+  const { control, errors, handleSubmit, onSubmit } = useAddCustomer();
   return (
     <div>
       <Header
